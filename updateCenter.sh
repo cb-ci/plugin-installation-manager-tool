b3e2451dede308cef6a786c4009364690d40bc03
cccaternberg <acaternberg@cloudbees.com>
2021-04-22 08:36:20 +0200

    rename

--- updateCenter.sh
+++ updateCenter.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 
-curl -d "script=$(cat /tmp/system-message-example.groovy)" \
+curl -d "script=$(cat ./ucStoreAndPromoteWithDependencies.groovy)" \
   -v --user username:ApiToken https://jenkins.example.com/scriptText \
  pluginName=job-dsl updateCenterAction=STORE
